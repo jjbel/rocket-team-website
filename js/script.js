5ff31fb21bde4a1dccd65ec6de6f6cbdf12a0c02
Jai Bellare <jaibellare@gmail.com>
2025-01-11 18:50:33 +0530

    run prettier formatting

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const logo = document.getElementById('logo');
+const logo = document.getElementById("logo");
 
-document.addEventListener('mousemove', (event) => {
+document.addEventListener("mousemove", (event) => {
   logo.style.transform = `translate(${event.pageX}px, ${event.pageY}px)`;
 });
